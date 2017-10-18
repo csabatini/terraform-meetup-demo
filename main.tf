374b23c3480c15c6ebe272cead0a4f121a7172c6
Caleb Sabatini <calebsabatini@gmail.com>
2017-10-18 14:30:13 -0500

    rename keypair resource

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   profile = "${var.profile}"
 }
 
-resource "aws_key_pair" "demo_ssh_keypairs" {
+resource "aws_key_pair" "demo_ssh_keypair" {
   key_name   = "demo_ssh_keypair"
   public_key = "${var.public_key}"
 }
@@ -11,7 +11,7 @@ resource "aws_key_pair" "demo_ssh_keypairs" {
 resource "aws_instance" "demo_instance" {
   ami           = "${var.amazon_linux_ami}"
   instance_type = "${var.instance_type}"
-  key_name      = "${aws_key_pair.demo_ssh_keypairs.key_name}"
+  key_name      = "${aws_key_pair.demo_ssh_keypair.key_name}"
   count         = 2
 
   tags {
